fc050a5e6bc05432973615ee4a18846d192ba866
m_masashi <drift_tms@yahoo.co.jp>
2022-09-17 23:24:09 +0900

    ベロシティのテスト作成。テストはまだ通ってない

--- ProductDevDomainLibTestProj/DevTeamTest.cs
+++ ProductDevDomainLibTestProj/DevTeamTest.cs
@@ -32,6 +32,24 @@ namespace ProductDevDomainLibTestProj
             Assert.Equal(new Feature("SAMPLE_ID"), output.Features[0]);
         }
 
+        [Fact]
+        public void Velocity3で1単位時間仕事をすると3Point分のアウトプットを出してくる()
+        {
+            var team = new DevTeam(velocity: 3, errorRate: 0);
+            
+            team.RequestFeature(new FeatureRequest("ID1"));
+            team.RequestFeature(new FeatureRequest("ID2"));
+            team.RequestFeature(new FeatureRequest("ID3"));
+            team.RequestFeature(new FeatureRequest("ID4"));
 
+            var output = team.Work();
+
+            var features = output.Features;
+            Assert.Equal(3, features.Count());
+            Assert.Equal(new Feature("ID1"), features[0]);
+            Assert.Equal(new Feature("ID2"), features[1]);
+            Assert.Equal(new Feature("ID3"), features[2]);
+
+        }
     }
 }
